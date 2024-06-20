8631d962568b91b9fc8effe87c652a2930fc6798
Zach <73715397+ZachVJ@users.noreply.github.com>
2024-06-20 18:29:03 -0400

    remove cors

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
-import cors from 'cors';
-import ytdl from 'ytdl-core';
+import cors from "cors";
+import ytdl from "ytdl-core";
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(cors('*'));
+// app.use(cors('*'));
 
-app.get('/check-download', async (req, res, next) => {
+app.get("/check-download", async (req, res, next) => {
   try {
     const { URL } = req.query;
     const {
@@ -26,28 +26,26 @@ app.get('/check-download', async (req, res, next) => {
   }
 });
 
-app.get('/download', async (req, res) => {
+app.get("/download", async (req, res) => {
   try {
-    const {
-      URL, downloadFormat, quality, title,
-    } = req.query;
-    if (downloadFormat === 'audio-only') {
+    const { URL, downloadFormat, quality, title } = req.query;
+    if (downloadFormat === "audio-only") {
       res.setHeader(
-        'Content-Disposition',
+        "Content-Disposition",
         `attachment; filename=${title.substring(0, 40)}.mp3`,
       );
       ytdl(URL, {
-        filter: format => format.container === 'm4a' && !format.encoding,
-        quality: quality === 'high' ? 'highest' : 'lowest',
+        filter: (format) => format.container === "m4a" && !format.encoding,
+        quality: quality === "high" ? "highest" : "lowest",
       }).pipe(res);
     } else {
       res.header(
-        'Content-Disposition',
+        "Content-Disposition",
         `attachment; filename="${title.substring(0, 25)}.mp4"`,
       );
       ytdl(URL, {
-        filter: downloadFormat === 'video-only' ? 'videoonly' : 'audioandvideo',
-        quality: quality === 'high' ? 'highestvideo' : 'lowestvideo',
+        filter: downloadFormat === "video-only" ? "videoonly" : "audioandvideo",
+        quality: quality === "high" ? "highestvideo" : "lowestvideo",
       }).pipe(res);
     }
   } catch (e) {
@@ -56,4 +54,6 @@ app.get('/download', async (req, res) => {
 });
 
 // eslint-disable-next-line no-console
-app.listen({ port: 8000 }, () => console.log('🚀 Server ready at http://localhost:8000'));
+app.listen({ port: 8000 }, () =>
+  console.log("🚀 Server ready at http://localhost:8000"),
+);
